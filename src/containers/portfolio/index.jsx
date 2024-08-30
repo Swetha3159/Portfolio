cb4d1cf396b5f6aeb59848983911e6d74b36b2f4
Swetha <swethareddy3159@gmail.com>
2024-08-30 11:35:50 +0530

    add resume portfolio and contact

--- src/containers/portfolio/index.jsx
+++ src/containers/portfolio/index.jsx
@@ -1,14 +1,130 @@
-import React from "react";
+import React, { useState } from "react";
 import PageHeaderContent from "../../components/pageHeaderContent";
 import { BsFillInfoCircleFill } from "react-icons/bs";
+import ImageOne from "../../images/image1.jpg";
+import ImageTwo from "../../images/image2.jpg";
+import ImageThree from "../../images/image3.jpg";
+import ImageFour from "../../images/image4.jpg";
+import ImageFive from "../../images/image5.jpg";
+import "./styles.scss";
 
+const PortfolioData = [
+  {
+    id: 2,
+    name: "Ecommerce",
+    image: ImageOne,
+    link: "",
+  },
+  {
+    id: 2,
+    name: "Shopping App",
+    image: ImageFive,
+    link: "",
+  },
+  {
+    id: 3,
+    name: "Notes App",
+    image: ImageTwo,
+    link: "",
+  },
+  {
+    id: 3,
+    name: "Supplier Design",
+    image: ImageThree,
+    link: "",
+  },
+  {
+    id: 2,
+    name: "Todo App",
+    image: ImageFour,
+    link: "",
+  },
+];
+
+const FilterData = [
+  {
+    filterId: 1,
+    label: "All",
+  },
+  {
+    filterId: 2,
+    label: "Development",
+  },
+  {
+    filterId: 3,
+    label: "Design",
+  },
+];
 const Portfolio = () => {
+  const [filterValue, setFilteredValue] = useState(1);
+  const [hoverValue, setHoverValue] = useState(null);
+
+  const handleFilter = (currentid) => {
+    setFilteredValue(currentid);
+  };
+  // console.log(filterValue);
+  const filteredItems =
+    filterValue === 1
+      ? PortfolioData
+      : PortfolioData.filter((item) => {
+          // console.log(item.id);
+          console.log(filterValue);
+          return item.id === filterValue;
+        });
+  // console.log(filteredItems);
+
+  const handleHover = (index) => {
+    setHoverValue(index);
+  };
+  // console.log(hoverValue);
   return (
     <section id="portfolio" className="portfolio">
       <PageHeaderContent
         headerText="My portfolio"
         icon={<BsFillInfoCircleFill size={40} />}
       />
+
+      <div className="portfolio__content">
+        <ul className="portfolio__content__filter">
+          {FilterData.map((item) => {
+            return (
+              <li
+                className={item.filterId === filterValue ? "active" : ""}
+                key={item.filterId}
+                onClick={() => handleFilter(item.filterId)}
+              >
+                {item.label}
+              </li>
+            );
+          })}
+        </ul>
+        <div className="portfolio__content__cards">
+          {filteredItems.map((item, i) => {
+            return (
+              <div
+                className="portfolio__content__cards__item"
+                key={`carditems${item.name.trim()}`}
+                onMouseEnter={() => handleHover(i)}
+                onMouseLeave={() => handleHover(null)}
+              >
+                <div className="portfolio__content__cards__item__image-wrapper">
+                  <a>
+                    <img src={item.image} alt={item.name} />
+                  </a>
+                </div>
+                <div className="overlay">
+                  {i === hoverValue && (
+                    <div>
+                      <p>{item.name}</p>
+                      <button>Visit</button>
+                    </div>
+                  )}
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </div>
     </section>
   );
 };
--- src/containers/resume/utils.jsx
+++ src/containers/resume/utils.jsx
@@ -2,37 +2,35 @@ export const data = {
   experience: [
     {
       title: "Junior Web Developer",
-      subTitle: "Banglore",
-      description: "Created User Templates using PHP and Wordpress",
+      subTitle: "Created User Templates using PHP and Wordpress",
+      experience: "2022 July - 2023",
     },
     {
       title: "Junior Web Developer",
-      subTitle: "Hyderabad",
-      description:
-        "Created Web applictions using Core Php and Larvel Framework",
+      subTitle: "Created Web applictions using Core Php and Larvel Framework",
+      experience: "2021 May - 2022 July",
     },
     {
       title: "Junior Web Developer",
-      subTitle: "Hyderabad",
-      description:
-        "Created Web applictions using Core Php and Larvel Framework",
+      subTitle: "Created Web applictions using Core Php and Larvel Framework",
+      experience: "2019 June - 2021 May",
     },
   ],
   education: [
     {
-      title: "Junior Web Developer",
-      subTitle: "Created User Templates using PHP and Wordpress",
-      experience: "2022 July - 2023 April",
+      title: "Secondary School",
+      subTitle: "Dilsukhnagar public school",
+      experience: "2012",
     },
     {
-      title: "Junior Web Developer",
-      subTitle: "Created Web applictions using Core Php and Larvel Framework",
-      experience: "2021 May - 2022 July",
+      title: "Intermediate Board",
+      subTitle: "Specialization in MPC",
+      experience: "2012-2014",
     },
     {
-      title: "Junior Web Developer",
-      subTitle: "Created Web applictions using Core Php and Larvel Framework",
-      experience: "2021 May - 2022 July",
+      title: "Bharat institute of engineering and technology",
+      subTitle: "BTech in Information Technology",
+      experience: "2014 - 2018",
     },
   ],
 };
